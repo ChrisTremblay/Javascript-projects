8c001e827fa7546e2a276bd033a6f7bb9a91307c
ChrisTremblay <tremblay.christo@gmail.com>
2020-06-24 20:51:09 +0200

    Add timer animation

--- Timer/timer.js
+++ Timer/timer.js
@@ -16,10 +16,10 @@ class Timer{
 
   start = () => {
     if(this.onStart){
-      this.onStart();
+      this.onStart(this.timeRemaining);
     }
     this.tick();
-    this.intervalID = setInterval(this.tick, 1000);
+    this.intervalID = setInterval(this.tick, 20);
   };
   
   pause = () => {
@@ -33,9 +33,9 @@ class Timer{
     if(this.timeRemaining <= 0){
       this.pause();
     }else{
-      this.timeRemaining = this.timeRemaining - 1;
+      this.timeRemaining = this.timeRemaining - 0.02;
       if(this.onTick){
-        this.onTick();
+        this.onTick(this.timeRemaining);
       }
     }
   };
@@ -45,6 +45,6 @@ class Timer{
   };
   
   set timeRemaining(time) {
-    this.durationInput.value = time;
+    this.durationInput.value = time.toFixed(2);
   }
 }
